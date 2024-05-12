d0e0c3d7fcadd59450b08feba957a0cf76033188
KaiseiWN10 <160144511+KaiseiWN10@users.noreply.github.com>
2024-05-12 22:53:37 +0100

    Update Particle.h

--- include/Particle.h
+++ include/Particle.h
@@ -9,7 +9,7 @@ struct Particle
 
   Particle(ngl::Vec3 _pos, ngl::Vec3 _dir, int _life, float _size=0.1f, ngl::Vec3 _colour={1,1,1}, float _calculated_properties=0.0f, ngl::Vec3 _gradient={1,1,1},
            ngl::Vec3 _pressure={0,0,0}) :
-      pos{_pos},dir{_dir},life{_life},size{_size},colour{_colour},calculated_properties{_calculated_properties},gradient{_gradient},pressure(_pressure)
+      pos{_pos},dir{_dir},life{_life},size{_size},colour{_colour},calculated_properties{_calculated_properties},gradient{_gradient},pressure{_pressure},aver_density{_aver_density}
       {
 
       }
@@ -23,6 +23,7 @@ struct Particle
   ngl::Vec3 gradient;
   ngl::Vec3 pressure;
   float density = 0.0f;
+  float aver_density = 1.0f;
   ngl::Vec3 press_Acc;
   std::vector<ngl::Vec3> neighParticles;
 };
