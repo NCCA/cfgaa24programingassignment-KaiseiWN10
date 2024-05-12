fd9fabb24c726982f87ec211854be3e04a5a4a9e
KaiseiWN10 <160144511+KaiseiWN10@users.noreply.github.com>
2024-05-12 22:56:20 +0100

    Update Particle.h

--- include/Particle.h
+++ include/Particle.h
@@ -8,7 +8,7 @@ struct Particle
   Particle()=default;
 
   Particle(ngl::Vec3 _pos, ngl::Vec3 _dir, int _life, float _size=0.1f, ngl::Vec3 _colour={1,1,1}, float _calculated_properties=0.0f, ngl::Vec3 _gradient={1,1,1},
-           ngl::Vec3 _pressure={0,0,0}) :
+           ngl::Vec3 _pressure={0,0,0}, float _aver_density=1.0f) :
       pos{_pos},dir{_dir},life{_life},size{_size},colour{_colour},calculated_properties{_calculated_properties},gradient{_gradient},pressure{_pressure},aver_density{_aver_density}
       {
 
