b1bf29274f25349000636a512ecb30a0d419982e
KaiseiWN10 <160144511+KaiseiWN10@users.noreply.github.com>
2024-05-18 21:55:42 +0100

    Update Particle.h

--- include/Particle.h
+++ include/Particle.h
@@ -8,9 +8,9 @@ struct Particle
   Particle()=default;
 
   Particle(ngl::Vec3 _pos, ngl::Vec3 _dir, int _life, float _size=0.1f, ngl::Vec3 _colour={1,1,1}, float _calculated_properties=0.0f, ngl::Vec3 _gradient={1,1,1},
-           ngl::Vec3 _pressure={0,0,0}, float _aver_density=1.0f, ngl::Vec3 _viscosity={1,1,1}, ngl::Vec3 _acceleration={1,1,1}) :
-      pos{_pos},dir{_dir},life{_life},size{_size},colour{_colour},calculated_properties{_calculated_properties},gradient{_gradient},pressure{_pressure},aver_density{_aver_density},
-      viscosity{_viscosity},acceleration{_acceleration}
+           ngl::Vec3 _pressure={0,0,0}, float _aver_density=1.0f, ngl::Vec3 _viscosity={1,1,1}, ngl::Vec3 _acceleration={1,1,1}, ngl::Vec3 _buoyancy={0,0,0}) :
+      pos{_pos},dir{_dir},life{_life},size{_size},colour{_colour},pressure{_pressure},aver_density{_aver_density},
+      viscosity{_viscosity},acceleration{_acceleration},buoyancy{_buoyancy}
       {
 
       }
@@ -20,15 +20,12 @@ struct Particle
   int life=100;
   float size=0.01f;
   bool isAlive = false;
-  float calculated_properties = 0.0f;
-  ngl::Vec3 gradient;
   ngl::Vec3 pressure;
   ngl::Vec3 viscosity;
   ngl::Vec3 acceleration;
+  ngl::Vec3 buoyancy;
   float density = 0.0f;
   float aver_density = 1.0f;
-  ngl::Vec3 press_Acc;
-  std::vector<ngl::Vec3> neighParticles;
 };
 
 #endif
